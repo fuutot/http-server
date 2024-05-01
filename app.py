b607f65c9ac354f4e250073fb449b143fd600ee2
fuutot <fuutt659@gmail.com>
2024-05-01 18:48:33 +0900

    update format html

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ class HelloServerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(index.encode('utf-8'))
+        html = index.format(
+            title = 'Hello from python',
+            message = 'Welcom to python'
+        )
+        self.wfile.write(html.encode('utf-8'))
         return
 
 server = HTTPServer(("", 8000), HelloServerHandler)
