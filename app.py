996cbe0d90950319993cfeffd01bb462db13c2b7
fuutot <fuutt659@gmail.com>
2024-05-01 19:22:30 +0900

    update routing

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+from urllib.parse import urlparse
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 with open('index.html', mode='r') as f:
     index = f.read()
+with open('next.html', mode='r') as f:
+    next = f.read()
 
 # BaseHTTPRequestHandlerを継承して独自のRequestHandlerを作る
 class HelloServerHandler(BaseHTTPRequestHandler):
 
+    # ルーティング
     def do_GET(self):
+        _url = urlparse(self.path)
+        if (_url.path == '/'):
+            self.index()
+        elif (_url.path == '/next'):
+            self.next()
+        else:
+            self.error()
+
+    def index(self):
         self.send_response(200)
         self.end_headers()
         html = index.format(
@@ -14,6 +27,20 @@ class HelloServerHandler(BaseHTTPRequestHandler):
             message = 'Welcom to python'
         )
         self.wfile.write(html.encode('utf-8'))
+        return
+    
+    def next(self):
+        self.send_response(200)
+        self.end_headers()
+        html = next.format(
+            title = 'Hi again',
+            message = 'This is next page'
+        )
+        self.wfile.write(html.encode('utf-8'))
+        return
+
+    def error(self):
+        self.send_error(404, "CANNOT ACCESS!!")
         return
 
 server = HTTPServer(("", 8000), HelloServerHandler)
