4dde232f1f9b5d02a353756aad098e4db55e1a64
fuutot <fuutt659@gmail.com>
2024-05-01 18:40:39 +0900

    update return index.html

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+with open('index.html', mode='r') as f:
+    index = f.read()
+
+# BaseHTTPRequestHandlerを継承して独自のRequestHandlerを作る
 class HelloServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'Sample web-server')
+        self.wfile.write(index.encode('utf-8'))
         return
 
 server = HTTPServer(("", 8000), HelloServerHandler)
